0bdfe46971f22e8e4339b55253b7b891622995c9
Nils Gawlik <nilsgawlik@gmx.de>
2018-10-06 16:53:52 +0200

    Implement camera controller

---
+++ PeaceJam2018/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Rigidbody2D following;
+	public float lerpFactor;
+
+	private void FixedUpdate() {
+		Vector3 pos = following.transform.TransformPoint((Vector3)following.centerOfMass);
+
+		transform.position = Vector3.Lerp(transform.position, pos, lerpFactor);
+	}
+}
