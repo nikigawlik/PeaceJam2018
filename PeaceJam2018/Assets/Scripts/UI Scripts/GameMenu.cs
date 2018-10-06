b3c7173480f42ce6e84fce6429225003f93063bf
Felix <felixszczesny@yahoo.fr>
2018-10-06 17:42:30 +0200

    Menu Scene with Assets and Scripts
    Menu Scene with Assets and Scripts

---
+++ PeaceJam2018/Assets/Scripts/UI Scripts/GameMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ PeaceJam2018/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+
+
+    public void ChangeTransform()
+    {
+        this.transform.localScale = Vector3.Lerp(this.transform.localScale, this.transform.localScale / 2, 20);
+        this.transform.position += Vector3.up;
+    }
+
+
+}
