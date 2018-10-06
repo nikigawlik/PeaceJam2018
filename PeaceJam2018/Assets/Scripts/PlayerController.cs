b714e2906db34d38feb81c0bc0a030eae7ab2715
Nils Gawlik <nilsgawlik@gmx.de>
2018-10-06 14:16:24 +0200

    Add player rotation

--- PeaceJam2018/Assets/Scripts/PlayerController.cs
+++ PeaceJam2018/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float acceleration;
 	public float maxSpeed;
+	public float rotationAcceleration;
+	public float rotationMaxSpeed;
 
     [HideInInspector]
     private float additionalMass = 0f;
@@ -32,7 +34,14 @@ public class PlayerController : MonoBehaviour {
 	private void FixedUpdate() {
 		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-		rb.AddForce(input * acceleration * (rb.mass + AdditionalMass));
+		// rb.AddForce(input * acceleration * (rb.mass + AdditionalMass));
+		rb.velocity += (input * acceleration * (rb.mass + AdditionalMass)) * Time.fixedDeltaTime;
 		rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+
+		float rotationAcc = (Input.GetAxis("RotateLeft") - Input.GetAxis("RotateRight")) * rotationAcceleration;
+
+		// rb.AddTorque(rotationAcc * rb.mass);
+		rb.angularVelocity += rotationAcc * Time.fixedDeltaTime;
+		rb.angularVelocity = Mathf.Clamp(rb.angularVelocity, -rotationMaxSpeed, rotationMaxSpeed);
 	}
 }
