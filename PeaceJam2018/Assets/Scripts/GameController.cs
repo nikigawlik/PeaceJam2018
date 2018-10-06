e2868bbb4d10cacee34231b16c0f5f5e1d832d58
Nils Gawlik <nilsgawlik@gmx.de>
2018-10-06 18:46:11 +0200

    Add end game menu

--- PeaceJam2018/Assets/Scripts/GameController.cs
+++ PeaceJam2018/Assets/Scripts/GameController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 	public GameObject player;
 	public Sprite[] colorWheel;
+	public float timer = 60;
+	public Text timerText;
+	public GameObject gameOverScreen;
+	public Text gameOverPeopleFoundText;
+	public Text gameOverPeopleNotFoundText;
+
 
     private static GameController instance;
 
@@ -26,5 +34,50 @@ public class GameController : MonoBehaviour {
 
     private void Start() {
 		Instance = this;
+		Time.timeScale = 1f;
+	}
+
+	private void Update() {
+		timer -= Time.deltaTime;
+		
+		// game recap
+		if(timer <= 0) {
+			timer = 0;
+			Time.timeScale = 0;
+
+			gameOverScreen.SetActive(true);
+
+			int numberOfPeople = Object.FindObjectsOfType<Sticky>().Length - 1;
+			int numberOfPeopleFound = player.GetComponentsInChildren<Sticky>().Length - 1;
+
+			gameOverPeopleFoundText.text = numberOfPeopleFound.ToString();
+			gameOverPeopleNotFoundText.text = (numberOfPeople - numberOfPeopleFound).ToString();
+		}
+
+		string str = "";
+
+		float minutes = Mathf.Floor(timer / 60);
+		float seconds = Mathf.RoundToInt(timer%60);
+		
+		if(minutes < 10) {
+			str += "0";
+		} 
+		str +=  minutes.ToString();
+		str +=  ":";
+		if(seconds < 10) {
+			str += "0";
+		}
+		str += Mathf.RoundToInt(seconds).ToString();
+		
+
+		timerText.text = str;
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void Quit() {
+		SceneManager.LoadScene("Menu");
 	}
 }
